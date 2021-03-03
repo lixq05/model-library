35367deecfeeaccdf89f01250d8e735f2752fc65
11349 <test@imooc.com>
2021-03-03 14:54:14 +0800

    1、使用Spring initializr创建spring cloud项目

---
+++ src/main/java/com/model/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package com.model.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
