651c32185ca9046c0b4872ecafe5ad463a3ae165
11349 <test@imooc.com>
2021-03-06 17:31:21 +0800

    前端登录页面和后台管理页面

---
+++ admin/src/router.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './views/login.vue'
+import Admin from './views/admin.vue'
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [{
+    path: '*',
+    redirect: "/login",
+  }, {
+    path: '/login',
+    component: Login
+  }, {
+    path: '/admin',
+    component: Admin
+  }]
+})
